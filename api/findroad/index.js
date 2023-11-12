4e657857e407403b7c751f761194154ecb834a14
Heejung Choi <71762328+hdddhdd@users.noreply.github.com>
2023-11-12 17:43:57 +0900

    안댐..

--- api/findroad/index.js
+++ api/findroad/index.js
@@ -1,6 +1,5 @@
 const fetch = require("node-fetch");
 
-
 module.exports = async function (context, req) {
     try {
         const { startX, startY, endX, endY } = req.body; // 매개변수로부터 위도, 경도 정보를 받음
@@ -32,17 +31,46 @@ module.exports = async function (context, req) {
             const json_response = await response.json();
             console.log(JSON.stringify(json_response, null, 2));
 
-            const totalFare = json_response.metaData.plan.itineraries[0].fare.regular.totalFare;
+            let itineraryText = '';
 
-            //코드 추가
-                
+            for (const itinerary of json_response['metaData']["plan"]["itineraries"]) {
+                const legs = itinerary.legs || [];
+            
+                for (const leg of legs) {
+                    if ("Lane" in leg) {
+                        const lanes = leg["Lane"];
+            
+                        console.log("Lanes:", lanes);
+                        
+                        for (let i = 0; i < lanes.length; i++) {
+                            const route = lanes[i].route || `No route information for Lane ${i}`;
+                            if (route.startsWith('SRT') || route.startsWith('KTX') || route.startsWith('ITX') || route.startsWith('무궁화')) {
+                                const departure_station = leg["start"]["name"];
+                                const arrival_station = leg["end"]["name"];
+            
+                                console.log(`야삐 Departure Station: ${departure_station}`);
+                                console.log(`야삐 Arrival Station: ${arrival_station}`);
+                                console.log(`야삐 Route: ${route}`);
+            
+                                itineraryText += `Departure Station: ${departure_station}\n`;
+                                itineraryText += `Arrival Station: ${arrival_station}\n`;
+                                itineraryText += `Route: ${route}\n\n`;
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            context.res.body = { itineraryText };
+            
             context.res = {
                 status: 200,
-                body: { text: `Total Fare: ${totalFare} 원` },
+                body: { itineraryText },  // 직접 context.res.body에 itineraryText를 넣어줌
                 headers: {
-                    'Content-Type': 'application/json'
+                  'Content-Type': 'application/json'
                 }
-            };
+              };
         } else {
             console.error(`API request failed with status code ${response.status}`);
             console.error(await response.text());
